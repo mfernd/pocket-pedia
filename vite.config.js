7bbabceeb1cc5909d58bf4d83177a9da4e1416f2
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-04-16 18:22:56 +0200

    fix: set base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_ACTIONS_BASE || undefined,
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
